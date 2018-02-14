24352086b6c1480ee65e252a5b230ea896efb233
jiangxingqi <xingqijiang@sohu-inc.com>
2018-02-14 16:49:32 +0800

    mvn profile

---
+++ src/main/java/com/weeking/share/config/Global.java
@@ -0,0 +1,36 @@
+package com.weeking.share.config;
+
+import com.google.common.collect.Maps;
+import com.weeking.share.util.PropertiesLoader;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Map;
+
+/**
+ * 全局配置类
+ * Created by jiangxingqi on 2017/7/27.
+ */
+public class Global {
+
+    /**
+     * 保存全局属性值
+     */
+    private static Map<String, String> map = Maps.newHashMap();
+
+    /**
+     * 属性文件加载对象
+     */
+    private static PropertiesLoader loader = new PropertiesLoader("jdbc.properties");
+
+    /**
+     * 获取配置
+     */
+    public static String getConfig(String key) {
+        String value = map.get(key);
+        if (value == null){
+            value = loader.getProperty(key);
+            map.put(key, value != null ? value : StringUtils.EMPTY);
+        }
+        return value;
+    }
+}
---
+++ src/test/java/TestProfile.java
@@ -0,0 +1,12 @@
+import com.weeking.share.config.Global;
+import org.junit.Test;
+
+public class TestProfile {
+
+    @Test
+    public void profile(){
+        String username= Global.getConfig("jdbc.username");
+        System.out.println(username);
+    }
+
+}
